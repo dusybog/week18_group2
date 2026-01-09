7b49eb4f5d0fa19c2a4d5842ebb35da8f922fb71
Lin Guo-Wei <168795815+Tron-04@users.noreply.github.com>
2026-01-09 11:31:48 +0800

    Add Bird class with gravity and jump logic

--- script.js
+++ script.js
@@ -23,6 +23,27 @@ let isPlaying = false;
 let isGravityInverted = false;
 
 
+const bird = {
+    x: 50, y: 150, width: 30, height: 30, velocity: 0, gravity: 0.25, jumpStrength: 5.5, color: '#0ff',
+    draw: function() {
+        ctx.fillStyle = this.color;
+        ctx.shadowBlur = 10; ctx.shadowColor = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.shadowBlur = 0;
+    },
+    update: function() {
+        let currentGravity = isGravityInverted ? -this.gravity : this.gravity;
+        this.velocity += currentGravity;
+        this.y += this.velocity;
+        if (this.y + this.height >= canvas.height || this.y <= 0) gameOver();
+    },
+    flap: function() {
+        let currentJump = isGravityInverted ? -this.jumpStrength : this.jumpStrength;
+        this.velocity = -currentJump; 
+    }
+};
+
+
 
 function loop() {
     if (!isPlaying) return;
@@ -34,3 +55,4 @@ function loop() {
 window.addEventListener('keydown', function(e) { if (e.code === 'Space') { if (!isPlaying && !isGameOver) resetGame(); else if (isPlaying) bird.flap(); } });
 canvas.addEventListener('click', function() { if (!isPlaying && !isGameOver) resetGame(); else if (isPlaying) bird.flap(); });
 restartBtn.addEventListener('click', resetGame);
+
